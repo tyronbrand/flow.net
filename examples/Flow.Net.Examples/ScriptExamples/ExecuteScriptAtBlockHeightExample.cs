207c2592fc189ef5a4c822a914a1a8754fefcf9d
Tyron Brand <tyron.brand@gmail.com>
2021-10-28 21:29:25 +0100

    output more information to console

--- examples/Flow.Net.Examples/ScriptExamples/ExecuteScriptAtBlockHeightExample.cs
+++ examples/Flow.Net.Examples/ScriptExamples/ExecuteScriptAtBlockHeightExample.cs
@@ -1,4 +1,6 @@
 ﻿using Flow.Net.Sdk;
+using Flow.Net.Sdk.Cadence;
+using System;
 using System.Threading.Tasks;
 
 namespace Flow.Net.Examples
@@ -15,7 +17,13 @@ namespace Flow.Net.Examples
         {
             var script = "pub fun main(): Int { return 1 + 2 }";
             var latestBlock = await _flowClient.GetLatestBlockAsync(); // getting a height for example purpose
-            await _flowClient.ExecuteScriptAtBlockHeightAsync(script.FromStringToByteString(), latestBlock.Height);
+            var response = await _flowClient.ExecuteScriptAtBlockHeightAsync(script.FromStringToByteString(), latestBlock.Height);
+            PrintResult(response);
+        }
+
+        private static void PrintResult(ICadence cadence)
+        {
+            Console.WriteLine($"Script result: {cadence.AsCadenceType<CadenceNumber>().Value}");
         }
     }
 }
--- examples/Flow.Net.Examples/ScriptExamples/ExecuteScriptAtLatestBlockExample.cs
+++ examples/Flow.Net.Examples/ScriptExamples/ExecuteScriptAtLatestBlockExample.cs
@@ -1,4 +1,6 @@
 ﻿using Flow.Net.Sdk;
+using Flow.Net.Sdk.Cadence;
+using System;
 using System.Threading.Tasks;
 
 namespace Flow.Net.Examples
@@ -14,7 +16,13 @@ namespace Flow.Net.Examples
         private static async Task Demo()
         {
             var script = "pub fun main(): Int { return 1 + 1 }";
-            var response = await _flowClient.ExecuteScriptAtLatestBlockAsync(script.FromStringToByteString());            
+            var response = await _flowClient.ExecuteScriptAtLatestBlockAsync(script.FromStringToByteString());
+            PrintResult(response);
+        }
+
+        private static void PrintResult(ICadence cadence)
+        {
+            Console.WriteLine($"Script result: {cadence.AsCadenceType<CadenceNumber>().Value}");
         }
     }
 }
