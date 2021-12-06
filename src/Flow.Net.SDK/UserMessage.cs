baa243a3f06e35a26030b4c0afe0674854ca9042
Tyron Brand <tyron.brand@gmail.com>
2021-12-06 22:42:06 +0000

    adding code comment

--- src/Flow.Net.SDK/UserMessage.cs
+++ src/Flow.Net.SDK/UserMessage.cs
@@ -4,6 +4,12 @@ namespace Flow.Net.Sdk
 {
     public static class UserMessage
     {
+        /// <summary>
+        /// Signs the full user message (UserDomainTag + message).
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="signer"></param>
+        /// <returns>Signed message as <see cref="byte[]" />.</returns>
         public static byte[] Sign(byte[] message, ISigner signer)
         {
             message = DomainTag.AddUserDomainTag(message);
