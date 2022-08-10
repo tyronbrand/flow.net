13979e254c6b8f872b1dfbcf05f59916bfd58f40
Tyron Brand <44845449+tyronbrand@users.noreply.github.com>
2022-08-10 14:42:34 +0100

    HTTP/gRPC clients use client options

---
+++ src/Flow.Net.Sdk.Client.Grpc/FlowGrpcClientOptions.cs
@@ -0,0 +1,10 @@
+﻿using Flow.Net.Sdk.Core.Client;
+using Grpc.Net.Client;
+
+namespace Flow.Net.Sdk.Client.Grpc
+{
+    public class FlowGrpcClientOptions : FlowClientOptions
+    {
+        public GrpcChannelOptions GrpcChannelOptions { get; set; }
+    }
+}
---
+++ src/Flow.Net.Sdk.Core/Client/FlowClientOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Flow.Net.Sdk.Core.Client
+{
+    public class FlowClientOptions : IFlowClientOptions
+    {
+        public string ServerUrl { get; set; }
+    }
+}
